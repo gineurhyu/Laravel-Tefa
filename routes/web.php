9f1a1d7022e1cbde85a5f55885df4689e243dd1c
gineurhyu <gineu.nurrahayu2008@smkn4-tsm.sch.id>
2024-10-18 11:23:29 +0700

    saya telah memperbarui E-poin saya

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterContoller;
+use App\Http\Controllers\Admin\AdminContoller;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register',[LoginRegisterController::class,'register'])->name('register');
+    Route::get('/store',[LoginRegisterController::class,'store'])->name('store');
+    Route::get('/login ',[LoginRegisterController::class,'login'])->name('login');
+    Route::get('/authenticate',[LoginRegisterController::class,'authenticate'])->name('authenticate');
+});
+
+Route::Middleware('auth','admin')->group(function () {
+    Route::get('admin/dashboard', [Adminroller::class,'index'])('admin/dashboard');
+    Route::get('logout', [LoginRegisterController::class,'logout'])('logout');
+
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +31,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
